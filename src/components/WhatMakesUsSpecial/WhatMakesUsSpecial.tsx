5283e44bbd120a4f7694172c363949cb7ec4d5ae
Bohdan <matula.bohdan@gmail.com>
2025-01-07 22:49:20 +0200

    created custom hook useOpacity

--- src/components/WhatMakesUsSpecial/WhatMakesUsSpecial.tsx
+++ src/components/WhatMakesUsSpecial/WhatMakesUsSpecial.tsx
@@ -1,36 +1,9 @@
 import { Link } from "react-router-dom"
 import styles from "./WhatMakesUsSpecial.module.scss"
-import { useEffect, useState, useRef } from "react";
+import { useOpacity } from "../../customHooks/useOpacity";
 
 export function WhatMakesUsSpecial() {
-    //little practice w hooks (opacity for absolute)
-    const [opacity, setOpacity] = useState(0);
-    const blockRef = useRef<HTMLDivElement | null>(null);
-
-    useEffect(() => {
-        const handleScroll = () => {
-            if (!blockRef.current) return;
-
-            const { top, height } = blockRef.current.getBoundingClientRect();
-            const windowHeight = window.innerHeight;
-
-            if (top + height < 0 || top > windowHeight) {
-                setOpacity(0);
-            } else {
-                const centerOffset = Math.abs(windowHeight / 2 - (top + height / 2));
-                const fadeZone = height / 2;
-
-                const newOpacity = 1 - Math.min(centerOffset / fadeZone, 1);
-                setOpacity(newOpacity);
-            }
-        };
-
-        window.addEventListener("scroll", handleScroll);
-        return () => {
-            window.removeEventListener("scroll", handleScroll);
-        };
-    }, []);
-
+    const { opacity, blockRef } = useOpacity();
     return (
         <section ref={blockRef} className={styles.special}>
             <div className={styles.special__container}>
