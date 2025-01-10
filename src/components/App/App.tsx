e15749a5d4602f1f064ab56d58065f67c7ee8b56
Bohdan <matula.bohdan@gmail.com>
2025-01-10 20:02:10 +0200

    added custom Scroll Restoration using sessionStorage

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,10 +4,12 @@ import styles from "./App.module.scss"
 import { ErrorPage } from "../ErrorPage/ErrorPage";
 import { HomePage } from "../HomePage/HomePage";
 import { FixedFooter } from "../FixedFooter/FixedFooter";
+import { ScrollRestoration } from "../ScrollRestoration/ScrollRestoration";
 
 export function App() {
   return (
     <Router>
+      <ScrollRestoration />
       <div className={styles.wrapper}>
         <FixedHeader />
         <main className={styles.main}>
